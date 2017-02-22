d0befbfeac6ad8c4fcd9cfbc616de84ada065044
yoshida0261 <yoshida261@gmail.com>
2017-02-23 03:01:45 +0900

    初期配置をRendererに移譲

--- app/src/main/java/com/starcompany/openglsample/DroidWars.java
+++ app/src/main/java/com/starcompany/openglsample/DroidWars.java
@@ -42,32 +42,11 @@ public class DroidWars implements  GLSurfaceView.Renderer{
     public DroidWars(Context context) {
         this.context = context;
         this.particleSystem = new ParticleSystem(300, 30);
-
         startNewGame();
     }
 
-    private void InitializeCharacter()
-    {
-        droid = new Droidkun(0, -0.5f, 0f, 0.5f, 0.02f, 0);
-
-        float y = 0.7f;
-        int count = 0;
-        for (int i = 0; i < TARGET_NUM; i++) {
-            enemies[i] = new Enemy(-0.9f +(0.16f * count), y, 0.1f, 0.1f, 0.02f, 0);
-            count += 1;
-            if(i == 11 || i == 23){
-                count = 0;
-                y += 0.15;
-            }
-        }
-
-        for(int i =0;i<BLOCK_NUM;i++){
-            blocks[i] = new Block(-0.75f + 0.3f * i , -0.8f, 0, 0.3f, 0.02f, 0);
-        }
-    }
-
     public void startNewGame() {
-        InitializeCharacter();
+        this.renderer.initializeCharacter();
         this.score = 0;
         this.startTime = System.currentTimeMillis();
         this.gameOverFlag = false;
